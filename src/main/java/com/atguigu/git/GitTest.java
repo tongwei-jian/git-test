32db9cf08d64cc64fb2a45c7b75684e1410b13ad
TongWeiJian <1273499565@qq.com>
2022-06-21 20:58:27 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello,git6!");
         System.out.println("hello,git7!");
         System.out.println("hello,git8!");
+        System.out.println("hello,git9!");
     }
 }
