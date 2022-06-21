c42555fcae423efdd2030cb9425f336644397d50
佟伟建 <11215642+tongweijian@user.noreply.gitee.com>
2022-06-21 12:59:34 +0000

    update src/main/java/com/atguigu/git/GitTest.java.

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello,git7!");
         System.out.println("hello,git8!");
         System.out.println("hello,git9!");
+        System.out.println("hello,git10!");
     }
 }
