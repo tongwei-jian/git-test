3cdca35a9d56b95644169c85a0a473bb153bf3ce
TongWeiJian <1273499565@qq.com>
2022-06-21 16:09:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author TongWeiJian
+ * @create 2022-06-21-16:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
